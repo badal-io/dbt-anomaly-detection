adf81b1e4c2a4124bfd5013dcd40086e77447b79
Rana Hashemi <rana.hashemi@loblaw.ca>
2023-02-09 12:01:19 -0500

    fixing some config errors

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,16 +9,8 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
+SELECT *
+FROM {{source('derived_web_3month', 'aggregations_main')}}
 
 /*
     Uncomment the line below to remove records with null `id` values
---
+++ target/compiled/anomaly_detection/models/example/my_first_dbt_model.sql
@@ -0,0 +1,18 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+
+
+SELECT *
+FROM `ld-snowplow`.`dbt_rhashemi`.`aggregations_main`
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
