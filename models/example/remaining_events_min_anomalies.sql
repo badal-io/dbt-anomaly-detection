cdaa0196a13bc3701d0efa44c2193a254a947cd0
Rana Hashemi <rana.hashemi@loblaw.ca>
2023-02-10 13:49:50 -0500

    removing trade off phase - step 2

--- models/example/remaining_events_min_anomalies.sql
+++ models/example/remaining_events_min_anomalies.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
 SELECT app_event, LoB, MIN(anomalies) AS anomalies 
-  FROM {{ref('filtered_model_features_dbt')}}
+  FROM {{ref('remaining_events_features_null_filtered')}}
   GROUP BY app_event, LoB
   ORDER BY app_event, LoB
--- models/example/trade_off_min_anomalies.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-
- SELECT app_event, LoB, MIN(anomalies) AS anomalies 
-  FROM {{ref('trade_off_phase')}}
-  GROUP BY app_event, LoB
-  ORDER BY app_event, LoB
