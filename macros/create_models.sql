ac57176b4a5482c622d9f2cc6396215c85e9418a
Artem Boiko <artem.boiko@badal.io>
2023-02-21 12:02:47 -0500

    Fix model creation

--- macros/create_models.sql
+++ macros/create_models.sql
@@ -2,24 +2,27 @@
 
     {% for model in var('models') %}
 
-    {{ log(model) }}
-    {{ print(model) }}
-
-    create or replace model `{{ target.database }}`.`{{ target.schema }}`.`derived_models_{{ model }}`
-    options(MODEL_TYPE="ARIMA_PLUS",TIME_SERIES_TIMESTAMP_COL="time_stamps",TIME_SERIES_DATA_COL="event_count",TIME_SERIES_ID_COL=['app_event', 'agg_tag'],HORIZON=120,HOLIDAY_REGION="CA")as (
-        
-SELECT
-  time_stamps,
-  event_count,
-  app_event,
-  agg_tag
-FROM
-  `ld-snowplow`.`dbt_anomaly_detection`.`aggregation_outliers_short`
-WHERE
-  DATE(time_stamps) >= DATE_SUB("2023-02-09", INTERVAL 60 DAY)
-  AND DATE(time_stamps) < DATE_SUB("2023-02-09", INTERVAL 15 DAY)
-  AND agg_tag = "{{ var('models')[model]['period'] }}"
-    );
+      create or replace model `{{ target.database }}`.`{{ target.schema }}`.`derived_models_{{ model }}`
+      options(
+        MODEL_TYPE="ARIMA_PLUS",
+        TIME_SERIES_TIMESTAMP_COL="time_stamps",
+        TIME_SERIES_DATA_COL="event_count",
+        TIME_SERIES_ID_COL=['app_event', 'agg_tag'],
+        HORIZON={{ var('anomaly_detection_horizon') }},
+        HOLIDAY_REGION="CA"
+      ) as (
+        SELECT
+          time_stamps,
+          event_count,
+          app_event,
+          agg_tag
+        FROM
+          `ld-snowplow`.`dbt_anomaly_detection`.`aggregation_outliers_{{ var('models')[model]['period_type'] }}`
+        WHERE
+          DATE(time_stamps) >= DATE_SUB({{ var('start_date') }}, INTERVAL {{ var('models')[model]['train_interval'] }} DAY)
+          AND DATE(time_stamps) < DATE_SUB({{ var('start_date') }}, INTERVAL {{ var('models')[model]['forecast_interval'] }} DAY)
+          AND agg_tag = "{{ var('models')[model]['period'] }}"
+      );
 
     {% endfor %}
 
--- models/example/derived_ml_detect.sql
+++ models/example/derived_ml_detect.sql
@@ -2,40 +2,48 @@
 {{ config(materialized='table', tags=["modelling"]) }}
 
   WITH test_set AS (
-  SELECT
-  time_stamps,
-        event_count,
-        app_event,
-        agg_tag
-      FROM
-        `ld-snowplow`.`dbt_anomaly_detection`.`all_agg_derived_cutoff`  
-      WHERE (agg_tag = "4hr" AND DATE(time_stamps) >= DATE_SUB("2023-02-09", INTERVAL 10 DAY)) 
-      OR (agg_tag = "8hr" AND DATE(time_stamps) >= DATE_SUB("2023-02-09", INTERVAL 10 DAY))
-      OR (agg_tag = "12hr" AND DATE(time_stamps) >= DATE_SUB("2023-02-09", INTERVAL 15 DAY))
-      OR (agg_tag = "24hr" AND DATE(time_stamps) >= DATE_SUB("2023-02-09", INTERVAL 15 DAY))
+    SELECT
+    time_stamps,
+          event_count,
+          app_event,
+          agg_tag
+        FROM
+          `ld-snowplow`.`dbt_anomaly_detection`.`all_agg_derived_cutoff`
+        WHERE
+
+      {% for model in var('models') %}
+
+        (agg_tag = "{{ var('models')[model]['period'] }}" AND DATE(time_stamps) >= DATE_SUB({{ var('start_date') }}, INTERVAL {{ var('models')[model]['forecast_interval'] }} DAY))
+
+        {{ " OR " if not loop.last }}
+
+      {% endfor %}
   )
 
     {% for threshold in var('anomaly_detection_prob_thresholds') %}
 
+      {% set outer_loop = loop %}
+
       {% for model in var('models') %}
 
         SELECT
-        app_event, agg_tag, 
-        time_stamps, 
-        "{{threshold}}" AS prob_threshold, 
-        "derived_models_{{ model }}" AS training_period, 
-        event_count, 
-        is_anomaly, 
-        lower_bound, 
-        upper_bound, 
-        anomaly_probability
+          app_event, agg_tag,
+          time_stamps,
+          "{{threshold}}" AS prob_threshold,
+          "derived_models_{{ model }}" AS training_period,
+          event_count,
+          is_anomaly,
+          lower_bound,
+          upper_bound,
+          anomaly_probability
         FROM
-      
-        ml.detect_anomalies(
-        model `{{ target.database }}`.`{{ target.schema }}`.`derived_models_{{ model }}`
-        struct( {{threshold}} as anomaly_prob_threshold),
-        (select * from (select * from test_set where agg_tag = "{{ var('models')[model]['period'] }}" ))
-        )
+          ml.detect_anomalies(
+            model `{{ target.database }}`.`{{ target.schema }}`.`derived_models_{{ model }}`,
+            struct( {{threshold}} as anomaly_prob_threshold),
+            (select * from (select * from test_set where agg_tag = "{{ var('models')[model]['period'] }}" ))
+          )
+
+        {{ "union all" if not loop.last or not outer_loop.last }}
 
       {% endfor %}
 
