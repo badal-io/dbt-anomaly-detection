15543e887fc59e8991b4ae70dfd7bed12a87d2f0
Rana Hashemi <rana.hashemi@loblaw.ca>
2023-02-23 17:13:48 -0500

    formatting

--- models/example/all_agg_derived.sql
+++ models/example/all_agg_derived.sql
@@ -1,34 +1,41 @@
 {{ config(materialized='table', tags=["data_preparation"]) }}
 
+
 {% for agg_level in var('anomaly_detection_aggregation_levels') %}
-SELECT "{{agg_level}}hr" AS agg_tag,
-  PARSE_TIMESTAMP("%F %H", CONCAT(date_trunc, ' ', CAST(_{{agg_level}}hr_trunc AS STRING))) AS time_stamps,
-  app_id,
-  event_type,
-  event_count
-FROM (
-  SELECT FORMAT_TIMESTAMP("%F", collector_tstamp) AS date_trunc,
-    FLOOR(CAST(FORMAT_TIMESTAMP("%H", collector_tstamp) AS INT64)/{{agg_level}})*{{agg_level}} AS _{{agg_level}}hr_trunc,
-    app_id,
-    event_type,
-    COUNT(event_type) AS event_count
-  FROM (
-    SELECT collector_tstamp, app_id,
-      CASE WHEN event = "unstruct" THEN user_event_name ELSE event
-      END AS event_type
-    FROM {{ref('reference_derived')}} )
-  GROUP BY
-    date_trunc,
-    _{{agg_level}}hr_trunc,
-    app_id,
-    event_type)
+
+
+ SELECT "{{agg_level}}hr" AS agg_tag,
+ PARSE_TIMESTAMP("%F %H", CONCAT(date_trunc, ' ', CAST(_{{agg_level}}hr_trunc AS STRING))) AS time_stamps,
+ app_id,
+ event_type,
+ event_count
+ FROM (
+
+
+   SELECT FORMAT_TIMESTAMP("%F", collector_tstamp) AS date_trunc,
+   FLOOR(CAST(FORMAT_TIMESTAMP("%H", collector_tstamp) AS INT64)/{{agg_level}})*{{agg_level}} AS _{{agg_level}}hr_trunc,
+   app_id,
+   event_type,
+   COUNT(event_id) AS event_count
+   FROM {{ref('reference_derived')}}
+   GROUP BY
+   date_trunc,
+   _{{agg_level}}hr_trunc,
+   app_id,
+   event_type)
+
+
 {% if not loop.last %}
 UNION ALL
 {% else %}
+
+
 ORDER BY
-  agg_tag,
-  time_stamps,
-  app_id,
-  event_type
+agg_tag,
+time_stamps,
+app_id,
+event_type
+
+
 {% endif %}
 {% endfor %}
--- models/example/reference_derived.sql
+++ models/example/reference_derived.sql
@@ -1,10 +1,4 @@
 {{ config(materialized='table', tags=["data_preparation"]) }}
-SELECT collector_tstamp, event, user_event_name, app_id
-FROM {{source('derived_app_event_1', 'web_purchase_exported')}}
-WHERE DATE(collector_tstamp) >= DATE_SUB({{ var('start_date') }}, INTERVAL {{ var('data_interval') }} DAY) AND DATE(collector_tstamp) < {{ var('start_date') }}
-
-UNION ALL
-
-SELECT collector_tstamp, event, user_event_name, app_id
-FROM {{source('derived_app_event_2', 'pco_web_apply_filter_exported')}}
+SELECT collector_tstamp, event_id, event_type, app_id
+FROM {{source('sampled_data', 'sample_table')}}
 WHERE DATE(collector_tstamp) >= DATE_SUB({{ var('start_date') }}, INTERVAL {{ var('data_interval') }} DAY) AND DATE(collector_tstamp) < {{ var('start_date') }}
