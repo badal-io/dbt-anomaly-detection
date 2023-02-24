df8f076af74cfc35cf8944ef0e9312213c9e012e
Rana Hashemi <rana.hashemi@loblaw.ca>
2023-02-24 12:44:37 -0500

    removing some models related to ads

--- models/example/ref_distinct_tuples.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table', tags=["config_selection"]) }}
-SELECT DISTINCT app_event
-FROM {{ref('filtered_model_features_dbt')}}
-WHERE app_event LIKE '%add%'
-UNION ALL
-SELECT DISTINCT app_event
-FROM {{ref('filtered_model_features_dbt')}}
-WHERE app_event NOT LIKE '%ad%'
-ORDER BY app_event
