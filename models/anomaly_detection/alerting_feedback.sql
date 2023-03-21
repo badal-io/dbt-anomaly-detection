47249d04194f417be31f3b4ede402fa1ab99e42f
Rana Hashemi <rana.hashemi@loblaw.ca>
2023-03-21 14:36:10 -0400

    adding alerting_feedback to lineage

---
+++ models/anomaly_detection/alerting_feedback.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table', tags=["config_selection"]) }}
+
+with empty_table as (
+    select
+        cast(null as TIMESTAMP) as time_stamps,
+        null as app_event,
+        null as control_config,
+        cast(null as int) as anomalies,
+        cast(null as float64) as RMSD_prcnt,
+        cast(null as float64) as event_count,
+        cast(null as float64) as forecast_value,
+        cast(null as float64) as lower_bound,
+        cast(null as float64) as upper_bound,
+        cast(null as float64) as anomaly_probability,
+        cast(null as boolean) as is_anomaly,
+        cast(null as int) as feedback_flag     
+)
+
+select * from empty_table
+-- This is a filter so we will never actually insert these values
+where 1 = 0
